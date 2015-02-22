c5eda2ed4e761fc9c9f86e82bca329b40e1b0a66
Luis Díaz Más <piponazo@gmail.com>
2015-02-22 19:16:55 +0100

    First version of Histogram1D

--- src/histogram1D.cpp
+++ src/histogram1D.cpp
@@ -1,13 +1,114 @@
 #include "histogram1D.h"
+#include "macros.h"
+
+#include <vector>
+#include <stdexcept>
 
 namespace ut
 {
 
-Histogram1D::Histogram1D(const double min, const double max, const std::uint32_t bins) :
-	_min (min)
-  , _max (max)
-  , _bins (bins)
+struct Histogram1D::Pimpl
 {
+	Pimpl(const double min_, const double max_, const std::uint32_t bins_) :
+		min (min_)
+	  , max (max_)
+	  , bins (bins_)
+	  , acc(bins, 0)
+	{
+	}
+
+	const double        min;
+	const double        max;
+	const std::uint32_t bins;
+	std::vector<double> acc;
+	double              nValues;
+};
+
+Histogram1D::Histogram1D(const double min, const double max, const std::uint32_t bins) : p(new Pimpl(min, max, bins))
+{
+}
+
+Histogram1D::~Histogram1D()
+{
+}
+
+double Histogram1D::getMin() const
+{
+	return p->min;
+}
+
+double Histogram1D::getMax() const
+{
+	return p->max;
+}
+
+std::uint32_t Histogram1D::getBins() const
+{
+	return p->bins;
+}
+
+std::uint32_t Histogram1D::nValues() const
+{
+	return static_cast<std::uint32_t>(p->nValues);
+}
+
+double Histogram1D::getRangeDistance() const
+{
+	return (p->max - p->min) / static_cast<double>(p->bins);
+}
+
+std::pair<double, double> Histogram1D::getRange(const std::uint32_t bin) const
+{
+	return std::make_pair(p->min + getRangeDistance() * bin,
+						  p->min + getRangeDistance() * (bin + 1) );
+}
+
+double Histogram1D::getPercentageBelow(const std::uint32_t bin) const
+{
+	checkBin(bin);
+	return accFromTo(0, bin, bin);
+}
+
+double Histogram1D::getPercentageAbove(const std::uint32_t bin) const
+{
+	checkBin(bin);
+	return accFromTo(bin, p->bins-1, bin);
+}
+
+double Histogram1D::getPercentage(const std::uint32_t bin) const
+{
+	return p->acc.at(bin) / p->nValues;
+}
+
+void Histogram1D::addValue(const double val)
+{
+	if (val < p->min || val > p->max) {
+		throw std::out_of_range("Value if out of the range of the histogram");
+	}
+
+	std::uint32_t bin = 0;
+	while ( (p->min + getRangeDistance() * (bin + 1)) <= val ) {
+		bin++;
+	}
+
+	p->acc[bin]+=1.;
+	p->nValues+=1.;
+}
+
+void Histogram1D::checkBin(const std::uint32_t bin) const
+{
+	if (bin > p->bins) {
+		throw std::out_of_range("Trying to access to non existing bin");
+	}
+}
+
+double Histogram1D::accFromTo(const std::uint32_t from, const std::uint32_t to, const std::uint32_t bin) const
+{
+	double acc = 0.;
+	for (std::uint32_t i = from; i <= to; ++i) {
+		acc += p->acc.at(bin) / p->nValues;
+	}
+	return acc;
 }
 
 }
--- tests/unitary/tests_histogram1d.cpp
+++ tests/unitary/tests_histogram1d.cpp
@@ -1,7 +1,85 @@
 #include "histogram1D.h"
 #include <gtest/gtest.h>
+#include <memory>
+
+using ut::Histogram1D;
+
+void checkBinRange(const Histogram1D &histo, const std::uint32_t bins)
+{
+	for (std::uint32_t i = 0; i < bins; ++i) {
+		const std::pair<double, double> range = histo.getRange(i);
+		const double inc = histo.getRangeDistance() * i;
+		const double first = histo.getMin() + inc;
+		const double second = first + histo.getRangeDistance();
+		ASSERT_DOUBLE_EQ(range.first, first);
+		ASSERT_DOUBLE_EQ(range.second, second);
+	}
+}
 
 TEST(Histogram1DTest, shouldCreateAndDestroyInstanceCorrectly)
 {
-	ut::Histogram1D histo(0., 10., 2);
+	Histogram1D histo(0., 10., 2);
+	ASSERT_DOUBLE_EQ(histo.getMin(), 0.);
+	ASSERT_DOUBLE_EQ(histo.getMax(), 10.);
+	ASSERT_EQ(histo.getBins(), 2);
+}
+
+TEST(Histogram1DTest, shouldObtainCorrectRangeDistance)
+{
+	std::unique_ptr<Histogram1D> histo = std::unique_ptr<Histogram1D>(new Histogram1D(0., 10., 2));
+	ASSERT_DOUBLE_EQ(histo->getRangeDistance(), 5.);
+
+	histo = std::unique_ptr<Histogram1D>(new Histogram1D(-5., 5., 2));
+	ASSERT_DOUBLE_EQ(histo->getRangeDistance(), 5.);
+
+	histo = std::unique_ptr<Histogram1D>(new Histogram1D(-10., 20., 2));
+	ASSERT_DOUBLE_EQ(histo->getRangeDistance(), 15.);
+
+	histo = std::unique_ptr<Histogram1D>(new Histogram1D(-5.5, 10., 8));
+	ASSERT_DOUBLE_EQ(histo->getRangeDistance(), 1.9375);
+}
+
+TEST(Histogram1DTest, shouldObtainCorrectBinRanges)
+{
+	std::unique_ptr<Histogram1D> histo = std::unique_ptr<Histogram1D>(new Histogram1D(0., 10., 5));
+	checkBinRange(*histo, 5);
+	histo = std::unique_ptr<Histogram1D>(new Histogram1D(-5.5, 10., 8));
+	checkBinRange(*histo, 8);
+}
+
+TEST(Histogram1DTest, shouldNotAddValuesOutOfRange)
+{
+	std::unique_ptr<Histogram1D> histo = std::unique_ptr<Histogram1D>(new Histogram1D(0., 5., 5));
+	ASSERT_THROW(histo->addValue(-0.0001), std::out_of_range);
+	ASSERT_THROW(histo->addValue(5.0001), std::out_of_range);
+}
+
+TEST(Histogram1DTest, shouldAddValuesAndCountThem)
+{
+	std::unique_ptr<Histogram1D> histo = std::unique_ptr<Histogram1D>(new Histogram1D(0., 5., 5));
+	for (std::uint32_t i = 0; i < 100; ++i) {
+		histo->addValue(i % 5);
+	}
+	ASSERT_EQ(histo->nValues(), 100);
+}
+
+TEST(Histogram1DTest, shouldAddSameNumberOfValuesForEachBinAndCheckPercentages)
+{
+	std::unique_ptr<Histogram1D> histo = std::unique_ptr<Histogram1D>(new Histogram1D(0., 5., 5));
+	for (std::uint32_t i = 0; i < 100; ++i) {
+		histo->addValue(i % 5);
+	}
+	for (std::uint32_t i = 0; i < 5; ++i) {
+		ASSERT_DOUBLE_EQ(histo->getPercentage(i), 0.2);
+		ASSERT_DOUBLE_EQ(histo->getPercentageBelow(i), 0.2 * (i+1));
+		ASSERT_DOUBLE_EQ(histo->getPercentageAbove(i), 1 - (0.2 * i));
+	}
+}
+
+TEST(Histogram1DTest, shouldThrowExceptionWhenTryingToAccessANonExistingBin)
+{
+	std::unique_ptr<Histogram1D> histo = std::unique_ptr<Histogram1D>(new Histogram1D(0., 10., 5));
+	ASSERT_THROW(histo->getPercentage(6), std::out_of_range);
+	ASSERT_THROW(histo->getPercentageBelow(6), std::out_of_range);
+	ASSERT_THROW(histo->getPercentageAbove(6), std::out_of_range);
 }
