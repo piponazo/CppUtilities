8b78ba7ee1ba95d43dd9b56214c6d6aef68019e7
Luis Díaz Más <piponazo@gmail.com>
2015-02-25 20:08:03 +0100

    Trying to use coveralls

--- travis/cmake.sh
+++ travis/cmake.sh
@@ -5,8 +5,8 @@ BUILD_DIR=build
 mkdir build && cd build
 
 if [ "$CC" == "gcc" ]; then
-    cmake $SOURCE_DIR -DBUILD_TESTS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+    cmake $SOURCE_DIR -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON
 else
-    cmake $SOURCE_DIR -DBUILD_TESTS=ON
+    cmake $SOURCE_DIR -DBUILD_TESTS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 fi
 
