354332e3a4383244a7b6863a3d3ebcd0f03d9ce5
Luis Díaz Más <piponazo@gmail.com>
2015-02-25 20:01:17 +0100

    Printing build mode

--- cmake_stuff/printInfo.cmake
+++ cmake_stuff/printInfo.cmake
@@ -1,5 +1,6 @@
 message( STATUS "------------------ General configuration for ${PROJECT_NAME} ${VERSION} -----------------")
 message( STATUS )
+message( STATUS "Build mode:                ${CMAKE_BUILD_TYPE}")
 message( STATUS "CMake Generator:           ${CMAKE_GENERATOR}")
 message( STATUS "Compiler:                  ${CMAKE_CXX_COMPILER_ID}")
 message( STATUS "C++ flags (Release):       ${CMAKE_CXX_FLAGS_RELEASE}")
