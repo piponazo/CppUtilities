ec8567a1129402c71d436e8473ebeb3ba3a6cd4e
Luis D�az M�s <ldiaz@catec.aero>
2015-08-10 20:12:18 +0200

    Enable compilation on windows with MSVC2013

--- cmake_stuff/compileroptions.cmake
+++ cmake_stuff/compileroptions.cmake
@@ -20,10 +20,21 @@ if (UNIX)
         set (CMAKE_CXX_FLAGS_DEBUG          "-g -O0 -DDEBUG")
 #        set (CMAKE_CXX_FLAGS_RELEASE        "-O2")
     endif()
+elseif(WIN32)
+   if(MSVC)
+      string(REGEX REPLACE "^  *| * $" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+      string(REGEX REPLACE "^  *| * $" "" CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
+      if(CMAKE_CXX_FLAGS STREQUAL CMAKE_CXX_FLAGS_INIT)
+  
+         # override cmake default exception handling option
+         string(REPLACE "/EHsc" "/EHa" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}"  CACHE STRING "Flags used by the compiler during all build types." FORCE)
+      endif()
+   endif()
 endif()
 
-if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" OR
-    ${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+if (${CMAKE_CXX_COMPILER_ID} STREQUAL GNU OR
+    ${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
 
    if(WARNINGS_ANSI_ISO)
       set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} -pedantic -Wextra")
@@ -42,7 +53,7 @@ if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" OR
 
 endif()
 
-IF (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+IF (${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
    execute_process(
    COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
    if (NOT (GCC_VERSION VERSION_GREATER 4.7 OR GCC_VERSION VERSION_EQUAL 4.7))
@@ -66,7 +77,7 @@ IF (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
         set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} --coverage")
     endif()
 
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
 
 #    if(NOT EXISTS ${CMAKE_CXX_COMPILER})
 #        message( FATAL_ERROR "Clang++ not found. " )
@@ -76,9 +87,35 @@ elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
         message(FATAL_ERROR "Coverage feature only supported by GCC.")
     endif()
 
-else ()
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
 
-    message( FATAL_ERROR "Not supported compiler: ${CMAKE_CXX_COMPILER_ID}. " )
+   set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} /wd4503 /wd4985 /wd4291 /W3")
+   set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} /MP /Qpar")
+   set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} /Gy") # Remove unreferenced functions: function level linking
+#   set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} /openmp")
+
+#   set(EXTRA_C_FLAGS_RELEASE "/MD /O2 /Ob2 /DNDEBUG") # Overriding default release flags
+#   set(EXTRA_C_FLAGS_RELEASE "${EXTRA_C_FLAGS_RELEASE} /Zi")# Produces PDBs & it implies /DEBUG.
+#   set(EXTRA_C_FLAGS_RELEASE "${EXTRA_C_FLAGS_RELEASE} /Oy-") # This is only available in x86
+# Disables the Frame-pointer omission. /Ox, /O1 & O2 implies /Oy. /Oy- disbles it. It improves the
+# debugging experience
+
+#set(EXTRA_C_FLAGS_RELEASE "${EXTRA_C_FLAGS_RELEASE} /Gm")# For minimal rebuilds (incompatible with /MP)
+   set(EXTRA_C_FLAGS_RELEASE "${EXTRA_C_FLAGS_RELEASE} /arch:AVX2")
+
+   set(EXTRA_C_FLAGS_DEBUG "${EXTRA_C_FLAGS_DEBUG} /DEBUG")
+
+   set(EXTRA_EXE_LINKER_FLAGS "${EXTRA_EXE_LINKER_FLAGS} /ignore:4049 /ignore:4217 /ignore:4099 /INCREMENTAL:NO")
+   set(EXTRA_EXE_LINKER_FLAGS_RELEASE "${EXTRA_EXE_LINKER_FLAGS_RELEASE} /DEBUG /OPT:REF /OPT:ICF /NODEFAULTLIB:libcmt")
+   set(EXTRA_EXE_LINKER_FLAGS_DEBUG "${EXTRA_EXE_LINKER_FLAGS_DEBUG} /OPT:NOREF /OPT:NOICF /NODEFAULTLIB:MSVCRT")
+
+   if(NOT MSVC_VERSION LESS 1400) # From VS 9.0 in advance
+          set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} /bigobj")
+   endif()
+
+   if(WARNINGS_ARE_ERRORS)
+          set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} /WX")
+   endif()
 
 endif()
 
